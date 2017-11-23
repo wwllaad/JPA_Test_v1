633f7f8420fb9dedc7f327c5a1b62e93cf1bae6e
wwllaad <wwllaad@mail.ru>
2017-11-23 17:36:09 +0300

    Initial commit

---
+++ src/main/java/com.tavrida.bve/model/Car.java
@@ -0,0 +1,58 @@
+package com.tavrida.bve.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year")
+    private int year;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public Car(int id, String name, int year) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/com.tavrida.bve/model/Seller.java
@@ -0,0 +1,58 @@
+package com.tavrida.bve.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "adress")
+    private String adress;
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+
+    public Seller(int id, String name, String adress) {
+        this.id = id;
+        this.name = name;
+        this.adress = adress;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
